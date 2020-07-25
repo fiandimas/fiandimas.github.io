d1984ccd18428b92e82a7404fb97086a63a13337
fiandimas <kidaliez@gmail.com>
2020-07-25 21:41:18 +0700

    update: re-create repo

---
+++ README.md
@@ -0,0 +1,1 @@
+fiandimas.github.io
---
+++ data.js
@@ -0,0 +1,33 @@
+const main = {
+  name: 'Alfian Dimas Sugara',
+  mail: 'alfndimas@gmail.com',
+  img: 'https://alchetron.com/cdn/asian-cat-66e2a381-ac52-494e-a700-a5e76bdff1d-resize-750.jpeg',
+  role: ['Backend Engineer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Home',
+      iconName: 'logo-firefox',
+      link: 'https://sivanesh-s.github.io'
+    },
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/fiandimas'
+    },
+    // {
+    //   name: 'Linkedin',
+    //   iconName: 'logo-linkedin',
+    //   link: 'https://www.linkedin.com/in/sivanesh-shanmugam/'
+    // },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'alfndimas@gmail.com'}?Subject=Hello!`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: '#projects' },
+    { name: 'Blogs', link: '#blogs' }
+  ]
+};
